054b8a238a6c40959f2c555f9c39c8f1ceab1db4
Felipe Dessoy Caraballo <63795029+FjjDessoyCaraballo@users.noreply.github.com>
2023-11-13 16:42:05 +0200

    Add files via upload
    corrected ft_lstnew.c

--- libft/ft_lstnew.c
+++ libft/ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: fdessoy- <fdessoy-@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 16:27:07 by fdessoy-          #+#    #+#             */
-/*   Updated: 2023/11/13 16:28:42 by fdessoy-         ###   ########.fr       */
+/*   Updated: 2023/11/13 16:35:19 by fdessoy-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ t_list	*ft_lstnew(void *content)
 {
 	t_list	*node;
 
-	node = (t_list *)malloc(sizeof(content));
+	node = (t_list *)malloc(sizeof(t_list));
 	if (!node)
 		return (NULL);
 	node->content = content;
