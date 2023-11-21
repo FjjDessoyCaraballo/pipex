1aac190b5ad227fde2dba0c870cb4d34bd5403d9
Felipe Dessoy Caraballo <63795029+FjjDessoyCaraballo@users.noreply.github.com>
2023-11-21 10:23:28 +0200

    Add files via upload

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,13 +6,13 @@
 /*   By: fdessoy- <fdessoy-@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 10:08:53 by fdessoy-          #+#    #+#             */
-/*   Updated: 2023/11/20 11:13:55 by fdessoy-         ###   ########.fr       */
+/*   Updated: 2023/11/20 16:33:41 by fdessoy-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memmove(void *dst, const void *src, size_t n)
+void	*ft_memmove(void *dst, const void *src, size_t len)
 {
 	unsigned char	*csrc;
 	unsigned char	*cdst;
